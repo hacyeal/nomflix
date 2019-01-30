73574dc48ac3b4f23329ecc40aafb2c3ed9e101c
hacyeal <hacyeal@naver.com>
2019-01-30 16:34:30 +0900

    React Router

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import Router from "Components/Router";
+import Header from "Components/Header";
 
 class App extends Component {
   render() {
     return (
-      <div className="App" />
+      <>
+      <Header />
+      <Router />
+      </>
     );
   }
 }
