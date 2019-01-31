cce260d9c509757160f6393441dd95ee496ccfe6
hacyeal <hacyeal@naver.com>
2019-01-31 16:46:34 +0900

    styling

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,9 +3,12 @@ import {BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
+import Header from "Components/Header";
 
 export default() => (
     <Router>
+        <>
+        <Header />
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/tv" component={TV} />
@@ -13,5 +16,6 @@ export default() => (
           <Route path="/search" component={Search} />
           <Redirect from="*" to="/" />
         </Switch>
+        </>
     </Router>
 )
